36daa59b8e1d4d20229b6997125323ca0e9442a8
MusangQim <musangqim03@gmail.com>
2025-11-12 20:15:34 +0800

    test file

--- oled_menu_list.ino
+++ oled_menu_list.ino
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
 #include <Wire.h>
-
+// ---
 // --- I²C OLED setup (SSD1306 128x64) ---
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
