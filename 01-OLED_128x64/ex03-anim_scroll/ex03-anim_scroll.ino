1b7bd82e8d38592708b3269862bd429ff5b4eda0
MusangQim <musangqim03@gmail.com>
2025-11-21 12:52:48 +0800

    update file

--- 01-OLED_128x64/ex03-anim_scroll/ex03-anim_scroll.ino
+++ 01-OLED_128x64/ex03-anim_scroll/ex03-anim_scroll.ino
@@ -1,9 +1,14 @@
+/*-Ju$t-NoT3s-
+kalau multiple animations occur at the same time, dia akan jittering
+*/
+
 #include <Arduino.h>
 #include <U8g2lib.h>
 
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
 int x = 128; //start dari Kanan
+int y = -10;
 
 void setup() 
 {
@@ -12,9 +17,32 @@ void setup()
 
 void loop() 
 {
+  // ------------------Vertical Scroll Animation----------------------------------------
   u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_6x10_tr);
 
-  
+  u8g2.drawStr(20, y, "WORLD!");
   u8g2.sendBuffer();
 
+  y++;
+  if (y > 64)
+  {
+    y = -10; //reset dari Atas
+  }
+  delay (20);
+/*
+  // ------------------Horizontol Scroll Animation----------------------------------------
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_6x10_tr); 
+  
+  u8g2.drawStr(x, 32, "HELLO!");
+  u8g2.sendBuffer();
+
+  x--; //gerak ke Kiri Terussss
+  if (x < -u8g2.getStrWidth("HELLO"))
+  {
+    x = 128;
+  }
+  delay (15); //control Speed animation
+*/
 }
