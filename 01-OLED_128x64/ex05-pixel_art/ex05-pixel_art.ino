8505c774ea32205a24ee8b5003fb14c4613f197b
MusangQim <musangqim03@gmail.com>
2025-11-24 23:54:58 +0800

    add files for ex05 learning

---
+++ 01-OLED_128x64/ex05-pixel_art/ex05-pixel_art.ino
@@ -0,0 +1,20 @@
+/*-Ju$t-NoT3s-
+Draw simple shapes guna pixels
+Fikir cara grid thinking - pixel ni mcm LEGO BLOCK
+guna BITMAPS to draw image efficient
+*/
+#include <Arduino.h>
+#include <U8g2lib.h>
+
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+void setup() 
+{
+  u8g2.begin();
+
+}
+
+void loop() 
+{
+
+}
