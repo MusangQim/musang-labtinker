b0a8cf5f34551ba6f2ba6b58bdffbf26b3ab218d
MusangQim <musangqim03@gmail.com>
2026-04-03 00:05:49 +0800

    update file for save data

--- web-todolist/script.js
+++ web-todolist/script.js
@@ -7,18 +7,29 @@ let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 function renderTasks() 
 {
     taskList.innerHTML = "";
-    tasks.forEach(function(task) 
+    tasks.forEach(function(task, index) 
     {
         const li = document.createElement("li");
-        li.textContent = task;
+        li.textContent = task.text;
+        li.addEventListener("click", function()
+        {
+            tasks[index].done = !tasks[index].done;
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+            renderTasks();
+        });
+        if (task.done)
+        {
+            li.style.textDecoration = "line-through";
+            li.style.opacity = "0.5";
+        }
         taskList.appendChild(li);
-    })
+    });
 }
 
 addBtn.addEventListener("click", function()
 {
     const taskText = taskInput.value;
-    tasks.push(taskText);
+    tasks.push({ text: taskText, done: false});
     localStorage.setItem("tasks", JSON.stringify(tasks));
     renderTasks();
     taskInput.value = "";
