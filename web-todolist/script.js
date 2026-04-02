4c6ba66d922631b77d6c58bae41b29b196909a2f
MusangQim <musangqim03@gmail.com>
2026-04-02 23:14:25 +0800

    learn how to save data input in localstorage

--- web-todolist/script.js
+++ web-todolist/script.js
@@ -2,11 +2,26 @@ const addBtn = document.getElementById("addBtn");
 const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
 
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function renderTasks() 
+{
+    taskList.innerHTML = "";
+    tasks.forEach(function(task) 
+    {
+        const li = document.createElement("li");
+        li.textContent = task;
+        taskList.appendChild(li);
+    })
+}
+
 addBtn.addEventListener("click", function()
 {
     const taskText = taskInput.value;
-    const li = document.createElement("li");
-    li.textContent = taskText;
-    taskList.appendChild(li);
+    tasks.push(taskText);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    renderTasks();
     taskInput.value = "";
 });
+
+renderTasks();
