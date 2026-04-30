6796c2509eb2239e2cec22f68f65da23478097c1
MusangQim <musangqim03@gmail.com>
2026-04-30 19:06:47 +0800

    update: try to do task tracker

---
+++ automate-tasktracker/task-cli.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import sys
+
+new_task = {
+    "id": len(tasks) + 1,
+    "description": "",
+    "status": ["todo", "in-progress", "done"],
+    "createdAt": "",
+    "updatedAt": "",
+}
+
+
+def handle_argument() -> None:
+    if len(sys.argv) > 1:
+        action = sys.argv[1]
+        if action == "add":
+            print("Add command detected")
+        elif action == "update":
+            print("Update command detected")
+        elif action == "list":
+            print("List command detected")
+        elif action == "delete":
+            print("Delete command detected")
+        else:
+            print("No command detected")
+    else:
+        print("No argument given")
+
+
+def main() -> None:
+    print("=== Task Tracker ===")
+    handle_argument()
+
+
+if __name__ == "__main__":
+    main()
+
+
+#for input_list in range(1, len(sys.argv)):
+        #    print(f"Task added: {sys.argv[input_list]}")
